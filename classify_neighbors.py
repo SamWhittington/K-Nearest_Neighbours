cb67b44d2df6e6719eb892c692893467233be007
SamWhittington <69531345+SamWhittington@users.noreply.github.com>
2020-10-14 12:12:50 +0100

    Create classify_neighbors.py

---
+++ classify_neighbors.py
@@ -0,0 +1,11 @@
+def classify(unknown, dataset, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
