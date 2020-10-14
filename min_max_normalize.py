e078a5fcac0dc3d9544cec0efd79f196979fc2b6
SamWhittington <69531345+SamWhittington@users.noreply.github.com>
2020-10-14 11:43:14 +0100

    Update and rename script.py to min_max_normalize.py

--- min_max_normalize.py
+++ min_max_normalize.py
@@ -1,5 +1,8 @@
+#Example Data
 release_dates = [1897, 1998, 2000, 1948, 1962, 1950, 1975, 1960, 2017, 1937, 1968, 1996, 1944, 1891, 1995, 1948, 2011, 1965, 1891, 1978]
 
+
+#Simple function for min-max normalization of data
 def min_max_normalize(lst):
   minimum = min(lst)
   maximum = max(lst)
